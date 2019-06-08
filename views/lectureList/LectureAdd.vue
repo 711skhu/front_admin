933ed01e0a49db6f98fd28869f36a7292e147b98
jeenie <jeenie828@gmail.com>
2019-06-08 21:04:42 +0900

    #9 강의 추가 컴포넌트 구현

---
+++ views/lectureList/LectureAdd.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="addLecture">
+    <div class="ui horizontal divider">
+      강의 추가
+    </div>
+    <div class="ui form success">
+      <div class="field">
+        <label>강의명</label>
+        <input type="text" placeholder="강의명을 입력해주세요.">
+      </div>
+      <div class="ui success message">
+        <div class="header">강의를 생성할 수 있습니다.</div>
+        <p>강의 추가 버튼을 클릭해 강의를 생성하세요.</p>
+      </div>
+      <div class="ui error message">
+        <div class="header">Action Forbidden</div>
+        <p>You can only sign up for an account once with a given e-mail address.</p>
+      </div>
+      <div class="ui submit button">강의 추가</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .addLecture {
+    margin: 0 15% 3% 15%;
+  }
+  .success {
+    background-color: white !important;
+  }
+  .ui.success.message {
+    background-color: #fcfff5 !important;
+  }
+</style>
--- views/lectureList/LectureListPage.vue
+++ views/lectureList/LectureListPage.vue
@@ -3,17 +3,20 @@
     <header-menu></header-menu>
     <h1 class="title">강의 목록</h1>
     <lecture-list></lecture-list>
+    <lecture-add></lecture-add>
   </div>
 </template>
 
 <script>
   import HeaderMenu from '@/components/header/HeaderMenu'
   import LectureList from '@/admin/views/lectureList/LectureList'
+  import LectureAdd from '@/admin/views/lectureList/LectureAdd'
 
   export default {
     components: {
       HeaderMenu,
-      LectureList
+      LectureList,
+      LectureAdd
     }
   }
 </script>
@@ -25,13 +28,6 @@
   .title {
     padding: 2% 15% 2% 16%;
     font-size: 25px;
-    font-weight: 800;
-  }
-
-  .addLectureLink {
-    float: right;
-    margin-right: 16%;
-    margin-top: -1%;
     font-weight: 800;
   }
 </style>
