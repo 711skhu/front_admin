dc6f0604704ec02f2baa4c3f037034ad362997a5
jeenie <jeenie828@gmail.com>
2019-06-08 23:35:16 +0900

    #9 강의 추가 수정

--- views/lectureList/LectureAdd.vue
+++ views/lectureList/LectureAdd.vue
@@ -4,16 +4,26 @@
       <div class="ui horizontal divider">
         강의 추가
       </div>
-      <div class="ui form success">
+      <div
+        class="ui form"
+        :class="{ success: isSuccess, error: isError}"
+      >
         <div class="field">
           <label>강의명</label>
-          <input type="text" v-model="newLectureName" placeholder="강의명을 입력해주세요.">
+          <input
+            type="text"
+            v-model="newLectureName"
+            placeholder="강의명을 입력해주세요.">
         </div>
-        <div class="ui success message">
+        <div
+          v-if="isSuccess"
+          class="ui success message">
           <div class="header">강의를 생성할 수 있습니다.</div>
           <p>강의 추가 버튼을 클릭해 강의를 생성하세요.</p>
         </div>
-        <div class="ui error message">
+        <div
+          v-if="isError"
+          class="ui error message">
           <div class="header">Action Forbidden</div>
           <p>You can only sign up for an account once with a given e-mail address.</p>
         </div>
@@ -38,7 +48,9 @@
       return {
         newLectureName:'',
         professorName: '이승진',
-        lectures: this.lectures
+        lectures: this.lectures,
+        //isSuccess: false,
+        error: false
       }
     },
     methods: {
@@ -49,6 +61,32 @@
           open: false
         })
         this.newLectureName = ''
+      }
+    },
+    computed: {
+      isError() {
+        if(this.newLectureName.length > 0) {
+          let newLecture = this.newLectureName;
+          this.lectures.forEach(function (item) {
+            if(item.title === newLecture){
+              console.log(item.title + 'vs' + newLecture)
+              return true;
+            }
+          })
+        }
+        return false;
+      },
+      isSuccess() {
+        if(this.newLectureName.length > 0) {
+          let newLecture = this.newLectureName;
+          this.lectures.forEach(function (item) {
+            if (item.title === newLecture) {
+              console.log(item.title === newLecture)
+              return false;
+            }
+            return true;
+          })
+        }
       }
     }
   }
